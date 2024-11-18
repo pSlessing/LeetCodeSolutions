12aaca3fda432ef4c929708ef310b8b94717775e
pSlessing <144026479+pSlessing@users.noreply.github.com>
2024-11-18 15:34:02 +0100

    Update 4. Median of Two Sorted Arrays.cs

--- 4. Median of Two Sorted Arrays.cs
+++ 4. Median of Two Sorted Arrays.cs
@@ -1,11 +1,15 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        
         int[] nums3 = new int[(nums1.Length + nums2.Length)];
         int nums1index = 0;
         int nums2index = 0;
 
+
+        //Do a partial merge of the two arrays
         for(int i = 0; i < ((nums1.Length + nums2.Length + 2) / 2); i++)
         {
+            //Make sure empty arrays are ignored
             if(nums1index < nums1.Length && nums2index < nums2.Length)
             {
                 if(nums1[nums1index] < nums2[nums2index])
@@ -31,6 +35,7 @@ public class Solution {
             }
         }
 
+        //Check total length of nums3 array, and return result based on that
         if((nums1.Length + nums2.Length) % 2 == 0)
         {
             int middleValue = nums3.Length / 2;
